55882fe00ceb474e1e203572648e8849e76734e0
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2018-02-13 13:26:21 -0500

    Leanred about accessor methods

--- csharp/treehouse/01-beginning-csharp/02-csharp-objects/Game/Invader.cs
+++ csharp/treehouse/01-beginning-csharp/02-csharp-objects/Game/Invader.cs
@@ -2,6 +2,16 @@ namespace TowerDefense
 {
     class Invader
     {
+        private MapLocation location;
 
+        public MapLocation GetLocation()
+        {
+            return this.location;
+        }
+
+        public void SetLocation(MapLocation value)
+        {
+            this.location = value;
+        }
     }
 }
